6303298f419493bc7317501e33476a655e4a5890
pauldereep <dereeper.paul@icloud.com>
2025-03-14 16:23:30 +0100

    test hf

--- hugging_face.py
+++ hugging_face.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_json("hf://datasets/pauldereep/test/modified_dataset.csv")
+df = pd.read_csv("hf://datasets/pauldereep/projet_cyber_ai/dataset.csv")
 
 print(df.head())
